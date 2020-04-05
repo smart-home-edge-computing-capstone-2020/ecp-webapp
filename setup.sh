ee32a5a3497fcddab69fefab7f4a4055fa1c4f2b
Niko Gupta <niko.gupta@gmail.com>
2020-04-05 10:07:43 -0400

    Add setup script
    The setup.sh script is intended to be run on an Ubuntu system as
    specified in the top level comment of the script. It will set up the
    environment so that both the frontend and backend are able to be run.

---
+++ setup.sh
@@ -0,0 +1,27 @@
+# Note: this script is intended for an Ubuntu vm.
+# Version: Ubuntu Server 18.04 LTS (HVM), 
+# AWS ami: ami-07ebfd5b3428b6f4d (64-bit x86) / ami-0400a1104d5b9caa1 (64-bit Arm)
+# I recommend an aws t2.micro instance
+
+# Set up top level modules
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3-venv
+sudo apt-get install python-pip
+
+# Set up the frontend environment
+sudo apt-get install npm
+sudo npm install -g npm@latest
+cd ecp-frontend
+sudo npm install
+#TODO: Need to go do npm install and stuff, clone repos
+
+# Set up the backend environment
+sudo apt-get install python3-flask
+cd ../flask-backend
+python3 -m venv my_venv
+source my_venv/bin/activate
+
+# Install requirements
+pip install --upgrade pip
+pip install -r requirements.txt
